5fd1672405ada421541b3ba41bb7bc1ee70031f3
Collin Person <collin.person@gmail.com>
2017-08-25 08:50:19 -0700

    add runtime error

---
+++ src/main/java/exceptions/ApiException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * Created by Guest on 8/25/17.
+ */
+public class ApiException extends RuntimeException {
+
+    private final int statusCode;
+
+    public ApiException (int statusCode, String msg){
+        super(msg);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
